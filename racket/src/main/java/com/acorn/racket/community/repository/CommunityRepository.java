9ee7616e1b957d64fd78f740fea3dee0b5d7236b
jgw <cjsgh967@gmail.com>
2024-05-23 19:25:19 +0900

    컴퓨터로옮겨

--- racket/src/main/java/com/acorn/racket/community/repository/CommunityRepository.java
+++ racket/src/main/java/com/acorn/racket/community/repository/CommunityRepository.java
@@ -1,5 +1,25 @@
 package com.acorn.racket.community.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.acorn.racket.community.controller.CommunityDetailDTO;
+
+@Repository
 public class CommunityRepository  implements CommunityRepositoryI {
 
+	@Autowired
+	private SqlSession session;
+	private static String ns ="com.acorn.boardMapper";
+	
+	public List<CommunityDetailDTO> SelectAll(){
+		
+		return session.selectList(ns+"selectAll");
+	}
+	
+	
+	
 }
--- racket/src/main/java/com/acorn/racket/community/repository/CommunityRepositoryI.java
+++ racket/src/main/java/com/acorn/racket/community/repository/CommunityRepositoryI.java
@@ -1,5 +1,27 @@
 package com.acorn.racket.community.repository;
 
+
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.acorn.racket.community.controller.CommunityDetailDTO;
+
+
+
+
+
+@Repository
 public interface CommunityRepositoryI {
 
+
+	
+		public static List<CommunityDetailDTO> selectAll(){
+			
+			return null;
+			
+			
+		}
 }
