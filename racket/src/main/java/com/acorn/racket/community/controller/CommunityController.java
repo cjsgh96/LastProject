6bbde1edb4406528ccf9d4915b7ece436b11a7c8
jgw <cjsgh967@gmail.com>
2024-05-23 15:21:06 +0900

    테스트

--- racket/src/main/java/com/acorn/racket/community/controller/CommunityController.java
+++ racket/src/main/java/com/acorn/racket/community/controller/CommunityController.java
@@ -1,5 +1,15 @@
 package com.acorn.racket.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class CommunityController {
 
+	@GetMapping("/test")
+	public String writeview() {
+		
+		return "CommunityWriter";
+	}
 }
